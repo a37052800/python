1abc2467a955bc85957ec3b4308e56ccbb2ff33f
a37052800 <a37052800@gmail.com>
2020-04-26 20:09:27 +0800

    a005 AC

--- ZeroJudge/a005.py
+++ ZeroJudge/a005.py
@@ -1,7 +1,16 @@
 for i in range(int(input())):
-    a = []
-    for j in range(4):
-        a.append(int(input()))
-    if(a[2]-a[1] == a[1]-a[0]):
-
-print(a)
+    a = input()
+    l = list()
+    for j in a.split(' '):
+        l.append(int(j.strip()))
+    # print(l)
+    if(l[2]-l[1] == l[1]-l[0]):
+        d = l[1]-l[0]
+        for j in range(5):
+            print(l[0]+d*j, end=' ')
+    else:
+        r = l[1]/l[0]
+        for j in range(4):
+            print(l[j],end=' ')
+        print(int(l[3]*r))
+    print('\n',end='')
---
+++ ZeroJudge/test.py
@@ -0,0 +1,9 @@
+a = input()
+l = list()
+for i in a.split(' '):
+    #print(i.strip())
+    l.append(int(i.strip()))
+
+"""for i in range(3):
+    l.append(input())"""
+print(l)
