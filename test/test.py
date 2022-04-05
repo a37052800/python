dbd1a65c54e656636cd4595a28b931f9dec977c4
Tim <39836309+a37052800@users.noreply.github.com>
2022-04-05 15:57:02 +0800

    0405

--- test/test.py
+++ test/test.py
@@ -1,20 +1,6 @@
-a=[0,0,0,0,0,0,0]
-sum=0
-for i in range(1,7):
-    print("請輸入第",i,"個學生資料")
-    a[i]=int(input())
-    sum+=a[i]
-ar=sum/6
-for i in range(1,7):
-    if a[i]-ar>=5:
-        print("A")
-        continue
-    if a[i]-ar>=2:
-        print("B")
-        continue
-    if a[i]-ar>=0:
-        print("C")
-        continue
-    else:
-        print("D")
-        continue
+def prF(type,num):
+    for i in range(num):
+        print(type)
+
+n = eval(input())
+prF("*",n)
